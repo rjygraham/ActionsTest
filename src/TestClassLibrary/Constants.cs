04b512f6ed8290608782204f2d1f2837ba334e51
Ryan Graham <rjygraham@live.com>
2021-03-14 14:20:01 -0400

    Testing SourceGenSecrets

---
+++ src/TestClassLibrary/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SourceGenSecrets;
+
+namespace TestClassLibrary
+{
+	public static partial class Constants
+	{
+		public static partial class Api
+		{
+			[SourceGenSecret(EnvironmentVariableName = "API_HOST_URL")]
+			public static string HostUrl { get; private set; }
+		}
+	}
+}
